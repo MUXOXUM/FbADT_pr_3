7fc64cc09c19645d06cc6dc990ed873aa60ee204
Name <alenon@example.com>
2025-12-16 09:53:02 +0300

    repair old stuff

--- services/php-web/laravel-patches/routes/web.php
+++ services/php-web/laravel-patches/routes/web.php
@@ -6,6 +6,7 @@ Route::get('/', fn() => redirect('/dashboard'));
 
 // Панели
 Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index']);
+Route::get('/iss',       [\App\Http\Controllers\IssController::class,       'index']);
 Route::get('/osdr',      [\App\Http\Controllers\OsdrController::class,      'index']);
 
 // Прокси к rust_iss
